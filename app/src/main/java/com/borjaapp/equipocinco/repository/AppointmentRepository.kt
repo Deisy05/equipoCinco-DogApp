110846a7748f65689c2d7ff2945cfdaa29661ddf
daniel98 <daniel.andres.caicedo@correounivalle.edu.co>
2024-05-13 12:49:11 -0500

    Update, Delete Database

--- app/src/main/java/com/borjaapp/equipocinco/repository/AppointmentRepository.kt
+++ app/src/main/java/com/borjaapp/equipocinco/repository/AppointmentRepository.kt
@@ -14,4 +14,22 @@ class AppointmentRepository (val context: Context){
             appointmentDao.saveAppointment(appointment)
         }
     }
+
+    suspend fun getListAppointment(): MutableList<Appointment> {
+        return withContext(Dispatchers.IO) {
+            appointmentDao.getListAppointment()
+        }
+    }
+
+    suspend fun deleteAppointment(appointment: Appointment){
+        withContext(Dispatchers.IO){
+            appointmentDao.deleteAppointment(appointment)
+        }
+    }
+
+    suspend fun updateRepository(appointment: Appointment){
+        withContext(Dispatchers.IO){
+            appointmentDao.updateAppointment(appointment)
+        }
+    }
 }
